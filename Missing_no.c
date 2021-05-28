f60104e76929e440832fb66a4301223cf8ab3d43
Haritha Harigovindan <42141035+hariharitha21@users.noreply.github.com>
2021-05-28 16:17:25 +0530

    Update Missing_no.c

--- Missing_no.c
+++ Missing_no.c
@@ -2,9 +2,9 @@
 int main() {
     int i=0,sum,total=0,res;
     int n=10; //change the value according to the question
-    int arr[9];
-    for(i=0;i<9;i++){
-        scanf("%d",&arr[i])
+    int arr[100];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }  
     sum=(pow(n,2)+n)/2; //sum of n natural numbers
     for(i=0;i<9;i++){
