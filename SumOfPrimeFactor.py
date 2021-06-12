34a6fdf89657b81c10fbb6ba7de38a37cef01f1b
Haritha Harigovindan <42141035+hariharitha21@users.noreply.github.com>
2021-06-12 12:14:29 +0530

    Create SumOfPrimeFactor.py

---
+++ SumOfPrimeFactor.py
@@ -0,0 +1,18 @@
+n=int(input())
+su=0
+arr=[]
+for i in range (2,n):
+    if n%i==0:
+        arr.append(i)
+for i in arr:
+    flag=0
+    for j in range (2,int(i)):
+        if int(i)%j==0:
+            flag=1
+            break
+    if flag==0:
+        if i*i==n:
+            su=su+(2*int(i))
+            break
+        su=su+int(i)
+print(su)
