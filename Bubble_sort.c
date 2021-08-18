ec120f804fad21b833e670f4acd4583f41cf8ef1
Haritha Harigovindan <42141035+hariharitha21@users.noreply.github.com>
2021-08-18 08:19:15 +0530

    Update Bubble_sort.c
    Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.

--- Bubble_sort.c
+++ Bubble_sort.c
@@ -4,6 +4,7 @@ int main(){
     scanf("%d",&n);
     for (i=0;i<n;i++)
         scanf("%d",&arr[i]);
+    //----------------------------Sorting starts here
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
             if(arr[i]>arr[j]){
