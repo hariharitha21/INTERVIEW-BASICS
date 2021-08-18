cdd200340c232326774e958ef963372cbf1ef41e
Haritha Harigovindan <42141035+hariharitha21@users.noreply.github.com>
2021-08-18 08:47:25 +0530

    Update Insertion_sort.c
    Insertion sort is a simple sorting algorithm that builds the final sorted array one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.

--- Insertion_sort.c
+++ Insertion_sort.c
@@ -7,6 +7,7 @@ int main() {
   printf("Unsorted Array:");
   for (i=0;i<n;i++)
         printf("%d",arr[i]);
+    //------Sorting starts here!
     for (i=1;i<n;i++){
         key=arr[i];
         j=i-1;
